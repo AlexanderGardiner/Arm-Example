cf76a2d9651f8c7223aaa216c56f7c7d3b00fc23
AlexanderGardiner <misc@alexandergardiner.com>
2024-09-02 15:27:16 -0700

    Inital commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,26 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+  // Create a variable for the arm subsystem here
+
+  /** This function is called at the start of robot code. */
+  @Override
+  public void robotInit() {
+    // Initalize the Arm Subsystem here
+  }
+
+  /** This function is called at the start of operator control. */
+  @Override
+  public void teleopInit() {
+    // Set the arm's setpoint here
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    // Log the arm's current position here
+  }
+
+}
---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSubsystem extends SubsystemBase {
+  // Create motor, encoder and PID variables here
+
+  // Instantiate motor, encoder and PID here
+  public ArmSubsystem() {
+
+  }
+
+  // Sets the arm's target position
+  public void setTargetPosition() {
+
+  }
+
+  // Gets the arm's current position
+  public double getCurrentPosition() {
+    return 0;
+  }
+}
